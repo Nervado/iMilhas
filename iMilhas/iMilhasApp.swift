fec48aac862e29602e4b3a7c4357e70a6a76489f
Nervado <evandroabreudeoliveira@gmail.com>
2022-11-06 20:31:32 -0300

    Initial Commit

---
+++ iMilhas/iMilhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMilhasApp.swift
+//  iMilhas
+//
+//  Created by Evandro  Abreu de Oliveira on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iMilhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
